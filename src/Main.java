afe421990026049aad06a705488ab1674e3f90d6
Soldatova <Ekats24@yandex.ru>
2023-03-29 16:05:40 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676, moneyAmount = 20;
+        int bonusAmount = ticketPrice / moneyAmount;
+        System.out.println(bonusAmount);
+    }
+}
